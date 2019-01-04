02b729ad9ca9a4a32526d4cd506ae652e721d1dd
elfein <elfein99@gmail.com>
2019-01-04 11:38:54 -0500

    Create dateId logic

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,6 +3,33 @@ import { Link } from 'react-router-dom'
 import { Container, Header, Divider, Button } from 'semantic-ui-react'
 
 export default class HomePage extends Component {
+    state = {
+        dateId: ''
+    }
+
+    getDateId = () => {
+        const date = new Date()
+        const day = date.getDate()
+        const month = date.getMonth()
+        let dateId = 0
+        for (let i = 0; i < month; i++) {
+            if (i === 4 || i === 6 || i === 9 || i === 11) {
+                dateId += 30
+            } else if (i === 2) {
+                dateId += 28
+            } else {
+                dateId += 31
+            }
+        }
+        dateId += day
+        return dateId
+    }
+
+    componentWillMount = async () => {
+        let dateId = await this.getDateId()
+        this.setState({ dateId })
+    }
+
     render() {
         return (
             <div>
@@ -12,7 +39,7 @@ export default class HomePage extends Component {
                     <Divider hidden />
                     <Divider />
                     <Divider hidden />
-                    <Link to='/'>
+                    <Link to={`/words/${this.state.dateId}`}>
                         <Button color='olive' size='massive'>Learn Today's Word</Button>
                     </Link>
                 </Container>
