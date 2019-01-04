3afb4c9b496e4ec3e18cb4c5835bd8df8903dad3
elfein <elfein99@gmail.com>
2019-01-04 11:41:13 -0500

    Route to word page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './components/HomePage'
+import WordPage from './components/WordPage'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
+        <Route exact path="/words/:dateId" component={WordPage} />
           <Route exact path="/" component={HomePage} />
         </Switch>
       </Router>
---
+++ client/src/components/WordPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class WordPage extends Component {
+  render() {
+    return (
+      <div>
+        Word Page
+      </div>
+    )
+  }
+}
