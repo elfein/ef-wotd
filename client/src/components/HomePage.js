0e55cb3818b0c590557375032a0d61e5c80090b5
elfein <elfein99@gmail.com>
2019-01-04 11:35:58 -0500

    Render home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { Container, Header, Divider, Button } from 'semantic-ui-react'
 
 export default class HomePage extends Component {
-  render() {
-    return (
-      <div>
-        Home Page
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Container textAlign='center'>
+                    <Divider hidden />
+                    <Header as='h1'>Welcome to Word-of-the-Day!</Header>
+                    <Divider hidden />
+                    <Divider />
+                    <Divider hidden />
+                    <Link to='/'>
+                        <Button color='olive' size='massive'>Learn Today's Word</Button>
+                    </Link>
+                </Container>
+            </div>
+        )
+    }
 }
