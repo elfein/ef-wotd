d65755cf0eed3781441fbda797b07e54a60249a9
elfein <elfein99@gmail.com>
2019-01-04 11:46:08 -0500

    Render word page and enable proxy

--- client/src/components/WordPage.js
+++ client/src/components/WordPage.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { Container, Header, Divider, Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 export default class WordPage extends Component {
-  render() {
-    return (
-      <div>
-        Word Page
-      </div>
-    )
-  }
+    state = {
+        word: {
+            name: '',
+            definition: ''
+        }
+    }
+
+    getWord = async () => {
+        const wordData = await axios.get(`/api/words/${this.props.match.params.dateId}`)
+        this.setState({ word: wordData.data[0] })
+    }
+
+    componentDidMount = () => {
+        this.getWord()
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.match.params.dateId !== this.props.match.params.dateId) {
+            this.getWord()
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Container textAlign='center'>
+                    <Divider hidden />
+                    <Header as='h1'>{this.state.word.name}</Header>
+                    <Divider hidden />
+                    <Header as='h2'>definition: {this.state.word.definition}</Header>
+                </Container>
+            </div>
+        )
+    }
 }
