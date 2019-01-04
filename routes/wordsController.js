02132c884115f6c46ab5567835146614377cfbf1
elfein <elfein99@gmail.com>
2019-01-04 11:21:58 -0500

    Enable show one route

--- routes/wordsController.js
+++ routes/wordsController.js
@@ -1,8 +1,15 @@
 const router = require('express').Router()
+const { Word } = require('../db/model')
 
 // index
 router.get('/', async (req, res) => {
     res.sendStatus(200)
+})
+
+// show one
+router.get('/:dateId', async (req, res) => {
+    const word = await Word.find({ dateId: req.params.dateId })
+    res.send(word)
 })
 
 module.exports = router
